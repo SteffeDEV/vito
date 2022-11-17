04bff176643f5e396cc01fc12d3a025107654ecf
SteffeDEV <95856241+SteffeDEV@users.noreply.github.com>
2022-11-17 19:09:25 +0200

    yh
    je

---
+++ amisvito/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game { 'gta5' }
+author 'JTK Modding'
+
+escrow_ignore {
+    '.meta',
+    'stream/.ytd'
+}
+
+files {
+    '*.meta'
+}
+
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+
+dependency '/assetpacks'
+dependency '/assetpacks'
